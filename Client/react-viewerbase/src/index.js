e34fb66683c41980f37aa0c8d0c3d2ebe2074b95
ajn404 <huiyuening00@gmail.com>
2020-10-20 13:17:41 +0800

    init react-struc

--- Client/react-viewerbase/src/index.js
+++ Client/react-viewerbase/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { LayoutButton } from 'react-viewerbase';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Example extends Component {
+  constructor(props) {
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.state = {
+      selectedCell: {
+        className: 'hover',
+        col: 1,
+        row: 1,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <LayoutButton
+        selectedCell={this.state.selectedCell}
+        onChange={cell => this.setState({ selectedCell: cell })}
+      />
+    );
+  }
+}
